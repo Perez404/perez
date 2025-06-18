628cd86fcfbe9444f9b851428b3f59d1d5dc2b04
Perez <daniil060304@gmail.com>
2025-06-18 08:18:05 +0300

    ff

---
+++ Production/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Production.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Production
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            try
+            {
+                App.DB = new Model.Entities();
+                MessageBox.Show("успешн подкл","попытка подкл к бд",MessageBoxButton.OK, MessageBoxImage.Information);
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("не удалось подкл", "попытка подкл к бд", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<Model.Material> materials = App.DB.Material.ToList();
+           
+            foreach (Model.Material mat in materials)
+            {
+                double amountNeeded;
+                List<Model.MaterialProduct> matprods = App.DB.MaterialProduct.Where(mp=>mp.MaterialId==mat.MaterialId).ToList();
+                amountNeeded = matprods.Sum(mp => mp.MaterialQuantityNeeded);
+                mat.AmountNeeded = Math.Round(amountNeeded, 2);  
+            }
+            lbMaterials.ItemsSource = materials;
+        }
+        private void lbMaterials_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Model.Material mat=lbMaterials.SelectedItem as Model.Material; 
+            View.EditMaterialWindow editMaterialWindow = new View.EditMaterialWindow(1,mat);
+            this.Hide();
+            editMaterialWindow.ShowDialog();
+            this.Show();
+        }
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Production/View/EditMaterialWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Production.View
+{
+    /// <summary>
+    /// Логика взаимодействия для EditMaterialWindow.xaml
+    /// </summary>
+    public partial class EditMaterialWindow : Window
+    {
+        int mode;
+        Model.Material mat;
+        //public EditMaterialWindow()
+        //{
+        //    InitializeComponent();
+        //}
+        public EditMaterialWindow(int mode, Model.Material mat )
+        {
+            InitializeComponent();
+            this.mode = mode;
+            this.mat = mat;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<Model.MaterialType> matTypes = App.DB.MaterialType.ToList();
+            cbType.ItemsSource = matTypes;
+            cbType.SelectedValuePath = "MaterialTypeId";
+            cbType.DisplayMemberPath = "MaterialTypeName";
+
+            if (mode==1)
+            {
+                tbTitle.Text = "редактирование материала";
+                btnFix.Content = "обновить материал";
+                cbType.SelectedValue=mat.MaterialTypeId;
+                tbName.Text = mat.MaterialName;
+                tbUnitPrice.Text=mat.MaterialUnitPrice.ToString();
+                tbQuantityInPack.Text=mat.MaterialQuantityInPack.ToString();
+                tbQuantityInStock.Text=mat.MaterialQuanityInStock.ToString();
+                tbMinQuantity.Text=mat.MaterialMinQuantity.ToString();
+            }
+            else
+            {
+                tbTitle.Text = "добавление материала";
+                btnFix.Content = "добавить материал";
+            }
+        }
+        private void btnFix_Click(object sender, RoutedEventArgs e)
+        {
+            mat.MaterialTypeId = (int)cbType.SelectedValue;
+            mat.MaterialName=tbName.Text;
+            
+            mat.MaterialUnitPrice=double.Parse(tbUnitPrice.Text);
+            tbQuantityInPack.Text = mat.MaterialQuantityInPack.ToString();
+            tbQuantityInStock.Text = mat.MaterialQuanityInStock.ToString();
+            tbMinQuantity.Text = mat.MaterialMinQuantity.ToString();
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
